28ee812be193cd388538c59efb1d9c1e9b99d553
Alex <avogelm@gmail.com>
2017-09-17 15:00:54 +0000

    main

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+
+//--Middleware
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var errorHandler = require('errorhandler');
+var path = require('path');
+var morgan = require('morgan');
+var port = require('./constants').APP_PORT;
+var apiRoutes = express.Router();
+
+//--Enviroment configuration
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(morgan('dev'));
+
+app.use('/api', apiRoutes);
+
+
+//---Proves per a depuracio
+app.get('/', function(req, res){
+	res.render('index', {serveraddress : require('./constants').SERVER_ADDRESS});
+});
+app.post('/', function(req, res){
+	res.status(200).send(req.body);
+});
+
+//---Enrutamiento de la aplicacion
+require('./routes')(app, apiRoutes);
+
+//---Error handling
+
+if('development' == app.get('env')){
+	app.use(errorHandler());
+}
+
+//---Server start
+app.listen(port);
+console.log('Servidor Express escuchando el puerto: '+port);
